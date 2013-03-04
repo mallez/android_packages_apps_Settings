db3b7fa0eedc773cce5c05a0320f8b148f39a1c8
mallez <github@pingt.net>
2013-03-04 14:22:54 +0100

    NavigationBar: Custom height (1/2)
    kanged from http://review.cyanogenmod.org/#/c/29636/
    
    All credits belongs to https://twitter.com/fitsnugly :-)
    
    Change-Id: I8e92720721f2c8e05a9b4804aacc068b6cba8682

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -21,10 +21,12 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.preference.ListPreference;
 import android.os.UserHandle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
 import android.view.IWindowManager;
 
@@ -34,13 +36,15 @@ import com.android.settings.SettingsPreferenceFragment;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SystemSettings extends SettingsPreferenceFragment {
+public class SystemSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
     private static final String TAG = "SystemSettings";
 
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
     private static final String KEY_HARDWARE_KEYS = "hardware_keys";
     private static final String KEY_NAVIGATION_BAR = "navigation_bar";
+    private static final String KEY_NAV_BUTTONS_EDIT = "nav_buttons_edit";
+    private static final String KEY_NAV_BUTTONS_HEIGHT = "nav_buttons_height";
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_STATUS_BAR = "status_bar";
     private static final String KEY_QUICK_SETTINGS = "quick_settings_panel";
@@ -49,6 +53,7 @@ public class SystemSettings extends SettingsPreferenceFragment {
 
     private PreferenceScreen mNotificationPulse;
     private PreferenceScreen mBatteryPulse;
+    private ListPreference mNavButtonsHeight;
     private boolean mIsPrimary;
 
     @Override
@@ -72,6 +77,15 @@ public class SystemSettings extends SettingsPreferenceFragment {
                     updateBatteryPulseDescription();
                 }
             }
+
+           /// NavButtons Height 
+           mNavButtonsHeight = (ListPreference) findPreference(KEY_NAV_BUTTONS_HEIGHT);
+           mNavButtonsHeight.setOnPreferenceChangeListener(this);
+
+           int statusNavButtonsHeight = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                 Settings.System.NAV_BUTTONS_HEIGHT, 48);
+           mNavButtonsHeight.setValue(String.valueOf(statusNavButtonsHeight));
+           mNavButtonsHeight.setSummary(mNavButtonsHeight.getEntry());
 
             // Only show the hardware keys config on a device that does not have a navbar
             // and the navigation bar config on phones that has a navigation bar
@@ -139,6 +153,19 @@ public class SystemSettings extends SettingsPreferenceFragment {
             mBatteryPulse.setSummary(getString(R.string.notification_light_disabled));
         }
      }
+
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mNavButtonsHeight) {
+            int statusNavButtonsHeight = Integer.valueOf((String) objValue);
+            int index = mNavButtonsHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.NAV_BUTTONS_HEIGHT, statusNavButtonsHeight);
+            mNavButtonsHeight.setSummary(mNavButtonsHeight.getEntries()[index]);
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public void onResume() {
